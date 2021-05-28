2269add4b183b9198c6c9f8598e7b567bb716a59
Zara Ahmadi <zatokey@gmail.com>
2021-05-28 02:49:05 -0400

    first styled-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-
+import {Button} from './common/Button'
 
 function App() {
   return (
     <div className="App">
      <h1>HEllo Wordl</h1>
+     <Button>ALERT</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/styles/index.scss';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
